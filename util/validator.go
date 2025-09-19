c48715781148f6142cf4fd5166a0ae975cf5a559
Alonza0314 <alonza.tu@gmail.com>
2025-09-19 09:03:04 +0000

    test: validate plmnid

--- util/validator.go
+++ util/validator.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"net"
+	"strconv"
 
 	"github.com/Alonza0314/free-ran-ue/model"
 	loggergoUtil "github.com/Alonza0314/logger-go/v2/util"
@@ -44,6 +45,32 @@ func ValidateIp(ip string) error {
 func ValidatePort(port int) error {
 	if port < 1 || port > 65535 {
 		return fmt.Errorf("invalid port range: %d, range should be 1-65535", port)
+	}
+	return nil
+}
+
+func ValidateIntStringWithLength(intString string, length int) error {
+	if _, err := strconv.Atoi(intString); err != nil {
+		return fmt.Errorf("invalid int string: %s", intString)
+	}
+	if len(intString) != length {
+		return fmt.Errorf("invalid int string: %s, length should be %d", intString, length)
+	}
+	return nil
+}
+
+func ValidatePlmnId(plmnId *model.PlmnIdIE) error {
+	if len(plmnId.Mcc) != 3 {
+		return fmt.Errorf("invalid mcc: %s, mcc should be 3 digits", plmnId.Mcc)
+	}
+	if err := ValidateIntStringWithLength(plmnId.Mcc, 3); err != nil {
+		return err
+	}
+	if len(plmnId.Mnc) != 2 {
+		return fmt.Errorf("invalid mnc: %s, mnc should be 2 digits", plmnId.Mnc)
+	}
+	if err := ValidateIntStringWithLength(plmnId.Mnc, 2); err != nil {
+		return err
 	}
 	return nil
 }
--- util/validator_test.go
+++ util/validator_test.go
@@ -116,3 +116,74 @@ func TestValidatePort(t *testing.T) {
 		})
 	}
 }
+
+var testValidateIntStringWithLengthCases = []struct {
+	name          string
+	intString     string
+	length        int
+	expectedError error
+}{
+	{
+		name:          "testValidIntString",
+		intString:     "12345",
+		length:        5,
+		expectedError: nil,
+	},
+	{
+		name:          "testInvalidIntString",
+		intString:     "12345a",
+		length:        5,
+		expectedError: fmt.Errorf("invalid int string: 12345a"),
+	},
+	{
+		name:          "testInvalidIntStringLength",
+		intString:     "12345",
+		length:        10,
+		expectedError: fmt.Errorf("invalid int string: 12345, length should be 10"),
+	},
+}
+
+func TestValidateIntString(t *testing.T) {
+	for _, testCase := range testValidateIntStringWithLengthCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := util.ValidateIntStringWithLength(testCase.intString, testCase.length)
+			assert.Equal(t, testCase.expectedError, err)
+		})
+	}
+}
+
+var testValidatePlmnIdCases = []struct {
+	name          string
+	plmnId        model.PlmnIdIE
+	expectedError error
+}{
+	{
+		name:          "testValidPlmnId",
+		plmnId:        model.PlmnIdIE{Mcc: "208", Mnc: "93"},
+		expectedError: nil,
+	},
+	{
+		name:          "testInvalidPlmnId",
+		plmnId:        model.PlmnIdIE{Mcc: "208", Mnc: "930"},
+		expectedError: fmt.Errorf("invalid mnc: 930, mnc should be 2 digits"),
+	},
+	{
+		name:          "testInvalidNonIntMcc",
+		plmnId:        model.PlmnIdIE{Mcc: "20a", Mnc: "93"},
+		expectedError: fmt.Errorf("invalid int string: 20a"),
+	},
+	{
+		name:          "testInvalidNonIntMnc",
+		plmnId:        model.PlmnIdIE{Mcc: "208", Mnc: "9a"},
+		expectedError: fmt.Errorf("invalid int string: 9a"),
+	},
+}
+
+func TestValidatePlmnId(t *testing.T) {
+	for _, testCase := range testValidatePlmnIdCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := util.ValidatePlmnId(&testCase.plmnId)
+			assert.Equal(t, testCase.expectedError, err)
+		})
+	}
+}
