b59d9b2bdc4f9a61c4dfb3b7e18a1ca2d7f32c04
Alonza0314 <alonza.tu@gmail.com>
2026-04-11 14:20:20 +0000

    fix: 3 digits mnc error

--- nasConvert_test.go
+++ nasConvert_test.go
@@ -9,28 +9,42 @@ import (
 
 var testSupiCases = []struct {
 	name           string
+	mccLength      int
+	mncLength      int
 	supi           string
 	expectedLength int
 	expectedBytes  []byte
 }{
 	{
 		name:           "imsi-208930000007487",
+		mccLength:      3,
+		mncLength:      2,
 		supi:           "208930000007487",
 		expectedLength: 13,
 		expectedBytes:  []byte{0x01, 0x02, 0xf8, 0x39, 0xf0, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x47, 0x78},
 	},
 	{
 		name:           "imsi-208930000000001",
+		mccLength:      3,
+		mncLength:      2,
 		supi:           "208930000000001",
 		expectedLength: 13,
 		expectedBytes:  []byte{0x01, 0x02, 0xf8, 0x39, 0xf0, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10},
+	},
+	{
+		name:           "imsi-001001000000001",
+		mccLength:      3,
+		mncLength:      3,
+		supi:           "001001000000001",
+		expectedLength: 13,
+		expectedBytes:  []byte{0x01, 0x00, 0x11, 0x00, 0xf0, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10},
 	},
 }
 
 func TestSupiToBytes(t *testing.T) {
 	for _, testCase := range testSupiCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			result := util.SupiToBytes(testCase.supi)
+			result := util.SupiToBytes(testCase.mccLength, testCase.mncLength, testCase.supi)
 			assert.Equal(t, testCase.expectedLength, len(result))
 			assert.Equal(t, testCase.expectedBytes, result)
 		})
